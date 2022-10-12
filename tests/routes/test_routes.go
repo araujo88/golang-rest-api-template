0249f18fa68593187c6c3b91626b7fa6126a15ff
Leonardo Araujo <leonardo.aa88@gmail.com>
2022-10-11 21:58:11 -0300

    fix: added PUT and DELETE test routes

--- tests/routes/test_routes.go
+++ tests/routes/test_routes.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	baseURL := "http://localhost:8001/api/v1"
 	book := []byte(`{"title": "1984", "author": "George Orwell"}`)
+	book_update := []byte(`{"title": "Animal farm", "author": "George Orwell"}`)
 
 	if TestGetRequest(baseURL+"/books") == http.StatusOK {
 		TestOK()
@@ -24,7 +25,25 @@ func main() {
 		TestFail()
 	}
 
-	if TestGetRequest(baseURL+"/books/1") == http.StatusOK {
+	if TestPostRequest(baseURL+"/books", book) == http.StatusCreated {
+		TestOK()
+	} else {
+		TestFail()
+	}
+
+	if TestGetRequest(baseURL+"/books/4") == http.StatusOK {
+		TestOK()
+	} else {
+		TestFail()
+	}
+
+	if TestPutRequest(baseURL+"/books/4", book_update) == http.StatusOK {
+		TestOK()
+	} else {
+		TestFail()
+	}
+
+	if TestDeleteRequest(baseURL+"/books/8") == http.StatusNoContent {
 		TestOK()
 	} else {
 		TestFail()
@@ -65,8 +84,7 @@ func TestGetRequest(url string) int {
 func TestPostRequest(url string, jsonBody []byte) int {
 	fmt.Printf("POST " + url)
 
-	bodyReader := bytes.NewReader(jsonBody)
-	r, err := http.Post(url, "application/json", bodyReader)
+	r, err := http.Post(url, "application/json", bytes.NewReader(jsonBody))
 
 	if err != nil {
 		log.Fatal(err)
@@ -80,8 +98,92 @@ func TestPostRequest(url string, jsonBody []byte) int {
 		bodyString := string(bodyBytes)
 		fmt.Printf(" Response: " + bodyString)
 	} else {
-		fmt.Printf("Error: status code %d", r.StatusCode)
+		fmt.Printf(" Error: status code %d", r.StatusCode)
 	}
 
 	return r.StatusCode
+}
+
+func TestPutRequest(url string, jsonBody []byte) int {
+	fmt.Printf("PUT " + url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(jsonBody))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.ContentLength = int64(bytes.NewBuffer(jsonBody).Len())
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		fmt.Printf(" Response: " + bodyString)
+	} else {
+		fmt.Printf(" Error: status code %d", res.StatusCode)
+	}
+
+	return res.StatusCode
+}
+
+// func TestPutRequest(url string, jsonBody []byte) int {
+// 	fmt.Printf("PUT " + url)
+
+// 	r, err := http.Put(url, "application/json", bytes.NewReader(jsonBody))
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if r.StatusCode == http.StatusOK {
+// 		bodyBytes, err := io.ReadAll(r.Body)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		bodyString := string(bodyBytes)
+// 		fmt.Printf(" Response: " + bodyString)
+// 	} else {
+// 		fmt.Printf(" Error: status code %d", r.StatusCode)
+// 	}
+
+// 	return r.StatusCode
+// }
+
+func TestDeleteRequest(url string) int {
+	fmt.Printf("DELETE " + url)
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusNoContent {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		fmt.Printf(" Response: " + bodyString)
+	} else {
+		fmt.Printf(" Error: status code %d", res.StatusCode)
+	}
+
+	return res.StatusCode
 }
